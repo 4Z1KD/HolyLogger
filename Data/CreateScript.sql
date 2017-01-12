6442eb85cb97d877dc2d4503704264eba66292f2
Gil (4Z1KD) <gil4z1kd@gmail.com>
2017-01-12 14:56:39 +0200

    cleanup - new files

---
+++ Data/CreateScript.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [qso] (
+  [Id] INTEGER NOT NULL
+, [my_callsign] nvarchar(100) NOT NULL
+, [my_square] nvarchar(100) NOT NULL
+, [frequency] nvarchar(100) NOT NULL
+, [band] nvarchar(100) NOT NULL
+, [dx_callsign] nvarchar(100) NOT NULL
+, [rst_rcvd] nvarchar(100) NOT NULL
+, [rst_sent] nvarchar(100) NOT NULL
+, [timestamp] nvarchar(100) NOT NULL
+, [mode] nvarchar(100) NOT NULL
+, [exchange] nvarchar(100) NULL
+, [comment] nvarchar(500) NULL
+, CONSTRAINT [PK_qso] PRIMARY KEY ([Id])
+);
